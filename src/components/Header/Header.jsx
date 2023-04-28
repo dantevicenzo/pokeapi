68f46c892f95d691b8b41dd0961d7ec2b785ada5
Dante Vicenzo <dantevcz@gmail.com>
2023-04-28 06:24:47 -0300

    new routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header>
         <img src={logo}></img>
         <ul>
-            <li><a href="/pokeapi/">Home</a></li>
+            <li><a href="/pokeapi/#/">Home</a></li>
             <li><a href="/pokeapi/#/pokedex">Pokédex</a></li>
             <li><a href="/pokeapi/#/legendaries">Legendaries</a></li>
             <li><a href="/pokeapi/#/documentation">Documentation</a></li>
