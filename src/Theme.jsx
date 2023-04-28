2daac3277c37ea895a2a8ecc3fddfc3e171ef215
Dante Vicenzo <dantevcz@gmail.com>
2023-04-28 14:18:35 -0300

    Theme effects

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,32 +1,43 @@
-// const theme = {
-//     colors: {
-//         third: '#F5DB13',
-//         primary: '#F2B807',
-//         second: '#F28F16',
-//         danger: '#D93E30',
-//         grey: '#F6F7F9',
-//         pharagraph: '#212121',
-//         whiteLight: '#FDFDFD',
-//         redColorGradient: 'linear-gradient(180deg, rgba(178,51,39,1) 0%, rgba(217,62,48,1) 100%);',
-//         redGradientCard: 'linear-gradient(180deg, rgba(115,33,25,1) 39%, rgba(217,62,48,1) 100%);',
-//         greyGradient: 'linear-gradient(180deg, rgba(255,255,255,1) 30%, rgba(245,245,245,1) 100%);',
-//         blueGradient: 'linear-gradient(180deg, rgba(91,199,250,1) 0%, rgba(53,186,255,1) 100%);',
-//         pinkGradient: 'linear-gradient(180deg, rgba(248,158,174,1) 0%, rgba(244,181,193,1) 100%);',
-//         greenGradient: 'linear-gradient(180deg, rgba(100,211,104,1) 0%, rgba(60,211,104,1) 100%);',
-//         goldGradient: 'linear-gradient(320deg, rgba(181,126,16,1) 0%, rgba(181,126,16,1) 25%, rgba(249,223,123,1) 40%, rgba(255,243,166,1) 60%, rgba(249,223,123,1) 75%, rgba(181,126,16,1) 100%);',
-//         yellowGradient: 'linear-gradient(180deg, rgba(245,219,19,1) 0%, rgba(242,184,7,1) 100%);',
-//     },
-//     fontSizes: {
-//         h1: '72px',
-//         h2: '32px',
-//         h3: '18px',
-//         h6: '12px',
-//         paragraph: '16px',
-//         cardTitleIMg: '36px',
-//         cardTitleContainer: '16px',
-//     },
-//     buttonSizes: {
-//         big: 'big',
-//         small: 'small',
-//     }
-// }
+const Theme = {
+    colors: {
+        third: '#F5DB13',
+        primary: '#F2B807',
+        second: '#F28F16',
+        danger: '#D93E30',
+        grey: '#F6F7F9',
+        pharagraph: '#212121',
+        whiteLight: '#FDFDFD',
+        redColorGradient: 'linear-gradient(180deg, rgba(178,51,39,1) 0%, rgba(217,62,48,1) 100%);',
+        redGradientCard: 'linear-gradient(180deg, rgba(115,33,25,1) 39%, rgba(217,62,48,1) 100%);',
+        greyGradient: 'linear-gradient(180deg, rgba(255,255,255,1) 30%, rgba(245,245,245,1) 100%);',
+        blueGradient: 'linear-gradient(180deg, rgba(91,199,250,1) 0%, rgba(53,186,255,1) 100%);',
+        pinkGradient: 'linear-gradient(180deg, rgba(248,158,174,1) 0%, rgba(244,181,193,1) 100%);',
+        greenGradient: 'linear-gradient(180deg, rgba(100,211,104,1) 0%, rgba(60,211,104,1) 100%);',
+        goldGradient: 'linear-gradient(320deg, rgba(181,126,16,1) 0%, rgba(181,126,16,1) 25%, rgba(249,223,123,1) 40%, rgba(255,243,166,1) 60%, rgba(249,223,123,1) 75%, rgba(181,126,16,1) 100%);',
+        yellowGradient: 'linear-gradient(180deg, rgba(245,219,19,1) 0%, rgba(242,184,7,1) 100%);',
+    },
+    fontSizes: {
+        h1: '72px',
+        h2: '32px',
+        h3: '18px',
+        h6: '12px',
+        paragraph: '16px',
+        cardTitleIMg: '36px',
+        cardTitleContainer: '16px',
+    },
+    buttonSizes: {
+        big: 'big',
+        small: 'small',
+    },
+    effectStyles: {
+        blur24: 'box-shadow: 4px 4px 24px rgb(1, 17, 38, 0.2);',
+        blur20: 'box-shadow: 4px 4px 20px rgb(1, 17, 38, 0.2);',
+        blur16: 'box-shadow: 4px 4px 16px rgb(1, 17, 38, 0.2);',
+        blur8: 'box-shadow: 4px 4px 8px rgb(1, 17, 38, 0.2);',
+        blurCard: 'box-shadow: 4px 4px 4px rgb(33, 33, 33, 0.10);',
+        filter: 'box-shadow: 2px 2px 2px rgb(33, 33, 33, 0.10);',
+        shadowButton: 'box-shadow: 0px 4px 8px rgb(0, 0, 0, 0.45);',
+    }
+}
+
+export default Theme;
