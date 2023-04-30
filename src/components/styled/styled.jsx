c540dc2233588083c0fc8111b954c957c67995bc
Dante Vicenzo <dantevcz@gmail.com>
2023-04-30 10:01:13 -0300

    PokemonItem component background

--- src/components/styled/styled.jsx
+++ src/components/styled/styled.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+// Home
+
 export const Header = styled.header`
     display: flex;
     justify-content: space-between;
@@ -36,7 +38,6 @@ export const Header = styled.header`
         text-decoration: underline;
     }
 `
-
 export const Footer = styled.footer`
     display: flex;
     justify-content: space-between;
@@ -47,7 +48,6 @@ export const Footer = styled.footer`
         font-weight: bold;
     }
 `
-
 export const Button = styled.button`
     background-color: ${ (props) => props.backgroundColor };
     height: 66px;
@@ -71,7 +71,6 @@ export const Button = styled.button`
         transform: scale(1.02);
     }
 `
-
 export const Main = styled.main`
     display: flex;
     flex-direction: row;
@@ -79,11 +78,12 @@ export const Main = styled.main`
     justify-content: space-between;
     flex: 1;
 `
-
 export const ImgBanner = styled.img`
     height: 75vh;
     object-fit: contain;
 `
+
+//404 Error Page
 
 export const NotFoundContainer = styled.div`
     padding: 3% 12%;
@@ -101,4 +101,80 @@ export const NotFoundContainer = styled.div`
     & span {
         font-size: 48px;
     }
+`
+
+// Pokedex
+
+export const PokemonItemContainer = styled.div`
+    /* height: 135px; */
+    /* width: 350px; */
+    background: linear-gradient(
+        to right,
+        ${ (props) => props.theme.colors.grey } 0%,
+        ${ (props) => props.theme.colors.grey } 39%,
+        ${ (props) => props.backgroundColor } 39%,
+        ${ (props) => props.backgroundColor } 100%
+    );
+    box-shadow: ${ (props) => props.theme.boxShadowStyles.blurCard };
+    border-radius: 12px;
+    padding: 0 27px;
+    display: flex;
+    flex-direction: row;
+    gap: 6px;
+`
+export const PokemonItemInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    padding: 6px 0;
+`
+export const PokemonItemTitle = styled.h3`
+    font-size: ${ (props) => props.theme.fontSizes.h3 };
+    text-shadow: ${ (props) => props.theme.boxShadowStyles.blurCard };
+    font-weight: bold;
+    line-height: 0;
+    margin-block-start: 0.6em;
+    margin-block-end: 1.2em;
+`
+export const PokemonItemPropsContainer = styled.div`
+    display: flex;
+    gap: 12px;
+`
+export const PokemonItemPropContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 6px;
+`
+export const PokemonItemPropTitle = styled.div`
+    color: #4B4B4B;
+    font-size: ${ (props) => props.theme.fontSizes.h6 };
+`
+export const PokemonItemPropValue = styled.div`
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+    border: 3px solid black;
+    border-radius: 50%;
+    text-align: center;
+    color: ${ (props) => props.theme.colors.pharagraph };
+    font-size: 15px;
+`
+export const PokemonItemTagsContainer = styled.div`
+    display: flex;
+    gap: 6px;
+`
+export const PokemonItemTag = styled.div`
+    background-color: ${ (props) => props.backgroundColor };
+    border-radius: 12px;
+    box-shadow: 0px -2px 0px rgb(0, 0, 0, 0.18) inset;
+    padding: 1px 12px;
+`
+export const PokemonItemImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const PokemonItemImg = styled.img`
+    height: 100%;
 `
